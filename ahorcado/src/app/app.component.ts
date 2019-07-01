ad9e72bf339c10bc1b823caaa0e6368679def189
Edison Alba <edison.alba@imaginamos.com.co>
2019-06-30 23:25:49 -0500

    add repeat

--- ahorcado/src/app/app.component.ts
+++ ahorcado/src/app/app.component.ts
@@ -10,7 +10,10 @@ export class AppComponent {
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  palabra = "AGUACATE";
+  palabraOculta = "_"; 
   constructor(){
-    console.log('ENTRO')
+    this.palabraOculta = "_  ".repeat(this.palabra.length)
+    
   }
 }
