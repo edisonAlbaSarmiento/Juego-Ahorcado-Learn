6f05088566babca8d86c2c5482d282e284703d55
Edison Alba <edison.alba@imaginamos.com.co>
2019-06-30 23:20:10 -0500

    add array of letters

--- ahorcado/src/app/app.component.ts
+++ ahorcado/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ahorcado';
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  constructor(){
+    console.log('ENTRO')
+  }
 }
