7494f0414c28846c105b840e5dad16a370ddc62d
Edison Alba <edison.alba@imaginamos.com.co>
2019-07-01 00:02:22 -0500

    add functions

--- ahorcado/src/app/app.component.ts
+++ ahorcado/src/app/app.component.ts
@@ -7,13 +7,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ahorcado';
+
   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
   'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
   'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
   palabra = "AGUACATE";
-  palabraOculta = "_"; 
+
+  palabraOculta = "";
+  
+  intentos = 0; 
+
+  gane = false;
+
+  perdio = false;
+
   constructor(){
-    this.palabraOculta = "_  ".repeat(this.palabra.length)
+    this.palabraOculta = "_ ".repeat(this.palabra.length)
     
+  }
+  comprobar(letra) {
+
+    this.existeLetra(letra);
+
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for (let i = 0; i < this.palabra.length; i++) {
+      if (this.palabra[i] === letra) {
+        palabraOcultaArr[i] = letra;
+      }
+    }
+    this.palabraOculta = palabraOcultaArr.join(' '); 
+    this.verificaGane();
+  }
+
+  verificaGane () {
+    const palabraArr = this.palabraOculta.split(' ');
+    const palabraEvaluar = palabraArr.join('');
+
+    if(palabraEvaluar === this.palabra) {
+      this.gane = true;
+      console.log('GANO');
+    } 
+
+    if (this.intentos >= 9) {
+      this.perdio = true;
+      console.log('Perdio');
+    }
+  }
+
+  existeLetra ( letra ) {
+    if ( this.palabra.indexOf(letra) >= 0) {
+      // console.log('LETRA EXISTE' + letra)
+    } else {
+      // console.log('LETRA NO EXISTE' + letra)
+      this.intentos ++;
+    }
   }
 }
